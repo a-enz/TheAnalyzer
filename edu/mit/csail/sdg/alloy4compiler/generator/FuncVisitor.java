ae7dbac6d8e00e861e4b4957b4f3b545e7f0821f
Andreas Enz <andi_enz@msn.com>
2014-04-16 21:31:10 +0200

    12/12 test cases. working on contracts

--- edu/mit/csail/sdg/alloy4compiler/generator/FuncVisitor.java
+++ edu/mit/csail/sdg/alloy4compiler/generator/FuncVisitor.java
@@ -158,8 +158,9 @@ public class FuncVisitor extends VisitQuery<Object> {
 			postNonNullCheck = false;
 		}
 		
-		if(postNonNullCheck) postconditions = "\n\t\tContract.Ensures(Contract.Result<" + returnType + ">() != null);";
+		if(postNonNullCheck && !isPred) postconditions += "\n\t\tContract.Ensures(Contract.Result<" + returnType + ">() != null);";
 		
+		if(!isPred && !(f.returnDecl instanceof ExprUnary)) postconditions += "\n\t\tContract.Ensures(" + f.returnDecl.accept(new ContractVisitor("Contract.Result<" + returnType + ">()")) + ");";
 		//Parameter Construction
 		List<Decl> declList = f.decls;
 		
--- edu/mit/csail/sdg/alloy4compiler/generator/tests23.als
+++ edu/mit/csail/sdg/alloy4compiler/generator/tests23.als
@@ -6,8 +6,8 @@ pred Implication[d0: Date, d1: Date, ds: some Date] {
   d0 = d1 implies (d0 in ds and d1 in ds)
 }
 
-assert Assertion {
-  0 = 1
-}
+
+assert Assertion { 
+0 = 1}
 
 check Assertion
